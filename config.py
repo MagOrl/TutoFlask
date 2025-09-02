6018b6c06009b6676e568c9522ab5393eee308cd
MagOrl <m4g0o0@proton.me>
2025-09-02 16:51:56 +0200

    fin du 'A vous de jouer'

--- config.py
+++ config.py
@@ -1,1 +1,3 @@
 SECRET_KEY='grbS>}U&oU&S/JK]_9X Ep%O'
+ABOUT = "Bienvenue sur la page à propos de Flask !"
+INFOS = "ARSAMERZOEV Magomed, 18 ans, célibataire et libre comme l'air"
--- monApp/views.py
+++ monApp/views.py
@@ -4,5 +4,13 @@ from .app import app
 def index():
     return "Hello world !"
 
+@app.route('/about')
+def about():
+    return app.config['ABOUT']
+
+@app.route('/contact')
+def contact():
+    return app.config['INFOS']
+
 if __name__== "__main__" :
     app.run()
