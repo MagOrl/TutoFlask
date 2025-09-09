20e0b15bcae2a4194e88b04ba78e0a7f67800076
MagOrl <m4g0o0@proton.me>
2025-09-09 16:19:56 +0200

    fin du TP2

--- monApp/app.py
+++ monApp/app.py
@@ -1,4 +1,10 @@
 from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
+
 app=Flask(__name__)
 
 app.config.from_object('config')
+
+# Create database connection object
+db = SQLAlchemy()
+db.init_app(app)
