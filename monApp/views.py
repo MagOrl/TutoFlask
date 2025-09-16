b7d281f501a686f12b0dd14df7f200819a288b4d
MagOrl <m4g0o0@proton.me>
2025-09-16 16:06:57 +0200

    fin du tp3

--- monApp/views.py
+++ monApp/views.py
@@ -1,16 +1,15 @@
 from .app import app 
 from flask import render_template
 @app.route('/')
-@app.route('/index/')
 def index():
     return render_template("index.html",title ="R3.01 Dev Web avec Flask",name="Cricri")
 @app.route('/about')
 def about():
-    return app.config['ABOUT']
+    return render_template("about.html",title ="à propos",name="Magomed")
 
 @app.route('/contact')
 def contact():
-    return app.config['INFOS']
+    return render_template("contact.html",name ="Magomed",contact = "magomed.arsamerzoev@etu.univ-orleans.fr",age = "19")
 
 if __name__== "__main__" :
     app.run()
