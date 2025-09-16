1244dae315798c03609a2a9d2427bc5c8666f72a
MagOrl <m4g0o0@proton.me>
2025-09-16 16:58:13 +0200

    début tp4

--- monApp/views.py
+++ monApp/views.py
@@ -1,8 +1,13 @@
 from .app import app 
-from flask import render_template
+from flask import render_template,request
+from monApp.models import Auteur
 @app.route('/')
 def index():
-    return render_template("index.html",title ="R3.01 Dev Web avec Flask",name="Cricri")
+    if len(request.args)==0:
+        return render_template("index.html",title="R3.01 Dev Web avec Flask",name="Cricri")
+    else :
+        param_name = request.args.get('name')
+        return render_template("index.html",title="R3.01 Dev Web avec Flask",name=param_name)
 @app.route('/about')
 def about():
     return render_template("about.html",title ="à propos",name="Magomed")
@@ -10,6 +15,11 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template("contact.html",name ="Magomed",contact = "magomed.arsamerzoev@etu.univ-orleans.fr",age = "19")
+
+@app.route('/auteurs/')
+def getAuteur():
+    lesAuteurs = Auteur.query.all().join().A 
+    return render_template('auteurs_list.html', title="R3.01 Dev Web avec Flask", auteurs=lesAuteurs)
 
 if __name__== "__main__" :
     app.run()
